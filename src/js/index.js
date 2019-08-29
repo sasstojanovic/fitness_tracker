561537f1134a65bbdfaa1f6d6563a5471ea3516f
Sasa Stojanovic <sasa.stojanovic@outlook.com>
2019-08-29 08:57:05 +0200

    First Page JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
 require('../sass/style.scss');
 
+var APP_CONFIG = {
+    daysToShow: 5,
+    oneStepDistance: 0.762,
+    oneStepBurnsCal: 0.05,
+    oneStepTime: 0.5
+}
+
+var dataStorage = {};
+
 var views = {
     week: {
         name: "week",
@@ -82,14 +91,107 @@ function initEvents() {
     document.getElementById("days-container").addEventListener('click', eventListeners.goToDayReview);
 }
 
+function setWeekAverageActivity(sortedDays) {
+    var totalSteps = 0;
+    var totalSeconds = 0;
+    var totalCalories = 0;
 
+    for (var i = 0; i < sortedDays.length; i++) {
+        totalSteps += dataStorage[sortedDays[i]].steps;
+    }
 
+    totalSeconds = totalSteps * APP_CONFIG.oneStepTime;
+    var hours = Math.floor(totalSeconds / 3600);
+    var minutes = Math.floor(totalSeconds % 3600 / 60);
+    totalCalories = Math.floor(totalSteps * APP_CONFIG.oneStepBurnsCal);
 
+    document.getElementById("activity-value").innerHTML = hours + "h" + "&nbsp;" + minutes + "min";
+    document.querySelectorAll("#steps-container .action-value")[0].innerHTML = totalSteps.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    document.querySelectorAll("#calories-container .action-value")[0].innerHTML = totalCalories.toString();
+}
 
+function fillDaysContainer() {
+    var daysContainerElement = document.getElementById("days-container");
+    var sortedDays = Object.keys(dataStorage).sort(function (a, b) { return dataStorage[b].epochDate - dataStorage[a].epochDate });
+    sortedDays.splice(APP_CONFIG.daysToShow, sortedDays.length - 1);
 
+    for (let i = sortedDays.length - 1; i >= 0; i--) {
+        var oneDayContainerElement = document.createElement("DIV");
+        oneDayContainerElement.className = "one-day-container";
+        var oneDayContentElement = document.createElement("DIV");
+        oneDayContentElement.className = "one-day-content";
+        oneDayContentElement.dataset.date = dataStorage[sortedDays[i]].epochDate;
+        oneDayContentElement.innerHTML = dataStorage[sortedDays[i]].date + "<br>" + dataStorage[sortedDays[i]].day.toUpperCase();
+        oneDayContainerElement.appendChild(oneDayContentElement);
+        daysContainerElement.appendChild(oneDayContainerElement);
+    }
+
+    setWeekAverageActivity(sortedDays);
+}
+
+function getData(url, successFunction, faultFunction) {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            successFunction(data);
+        } else {
+
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function processData(data) {
+    var weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    if (data) {
+        for (var i = 0; i < data.length; i++) {
+            var dayEpoch = null;
+            var steps = null;
+            var thisDay = null;
+            var thisDate = null;
+
+            if (data[i].timestamp) {
+                var currentDate = new Date(data[i].timestamp);
+                thisDate = currentDate.getDate();
+                thisDay = weekdays[currentDate.getDay()];
+                currentDate = currentDate.setHours(0, 0, 0, 0);
+                dayEpoch = String(currentDate.valueOf());
+            }
+
+            if (data[i].steps) {
+                steps = data[i].steps;
+            }
+
+            if (dataStorage.hasOwnProperty(dayEpoch)) {
+                dataStorage[dayEpoch].steps += steps;
+            } else {
+                dataStorage[dayEpoch] = {
+                    "epochDate": Number(dayEpoch),
+                    "steps": steps,
+                    "date": thisDate,
+                    "day": thisDay
+                }
+            }
+        }
+        console.log("Data: " + JSON.stringify(dataStorage, null, 4));
+    }
+
+    fillDaysContainer();
+}
+
+function errorFunction() {
+
+}
 
 function onPageLoad() {
+    var url = "https://api.myjson.com/bins/1gwnal";
     initEvents();
+    getData(url, processData, errorFunction);
 }
 
 onPageLoad();
